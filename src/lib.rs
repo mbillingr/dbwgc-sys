de640477542bf88da2f0d2e71eb1494be45d84c9
Martin Billinger <flkazemakase@gmail.com>
2019-09-10 22:10:10 +0200

    export allocator

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,20 @@ pub use gc::*;
 
 #[link(name = "gc")]
 extern {}
+
+use std::alloc::{GlobalAlloc, Layout, alloc};
+use std::ptr::null_mut;
+
+pub struct DbwGcAllocator;
+
+unsafe impl GlobalAlloc for DbwGcAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let ptr = GC_malloc(layout.size()) as *mut _;
+        debug_assert_eq!(ptr as usize % layout.align(), 0);
+        ptr
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+
+    }
+}
